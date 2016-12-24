3c9009c7886906a92203136f0af5d9d2374eae25
SugarPocket Server <kittisak88888job@gmail.com>
2016-12-24 19:44:53 +0700

    แก้ไขข้อผิดพลาด / Fix bug

--- src/ScoreboardStats/Main.php
+++ src/ScoreboardStats/Main.php
@@ -8,34 +8,36 @@ use pocketmine\Server;
 use pocketmine\scheduler\CallbackTask;
 
 class Main extends PluginBase implements Listener {
- 	private $timer, $EconomyS, $PurePerms;
+ 	private $timer, $EconomyS; //$PurePerms;
  	
  
  	public function onEnable() {
  		$this->getServer()->getPluginManager()->registerEvents($this, $this);
  		$this->EconomyS = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
-                $this->PurePerms = $this->getServer()->getPluginManager()->getPlugin("PurePerms");
- 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new CallbackTask(array($this, "ScoreboardStats")), 10);
-		$this->getLogger()->info("§a Plugin Status Runing");
+                //$this->PurePerms = $this->getServer()->getPluginManager()->getPlugin("PurePerms");
+ 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new CallbackTask(array($this, "Status")), 20);
+		$this->getLogger()->info("§a ปลักอิน แสดงสถานะ ถูกเปิด");
+		$this->getLogger()->info("§e กรุณลง ปลักอิน Economy API ด้วย");
+		$this->getLogger()->info("§9 เซิฟเวอร์ที่ใช้ปลักอินนี้ sgpk.ddns.net Port 19132");
  		$this->timer = 0;
  	}
  
- 	public function ScoreboardStats() {
+ 	public function Status() {
  		foreach($this->getServer()->getOnlinePlayers() as $players) {
  			$Name = $players->getPlayer()->getName();
  			$Money = $this->EconomyS->mymoney($Name);
                         $Group = $this->PurePerms->getUser($player)->getGroup($levelName); 
- 			//$Online = count(Server::getInstance()->getOnlinePlayers());
+ 			$Online = count(Server::getInstance()->getOnlinePlayers());
  			//$Full = $this->getServer()->getMaxPlayers();
  			//$TPS = $this->getServer()->getTicksPerSecond(); 
  			//$Load = $this->getServer()->getTickUsageAverage();
  			//$Time = intval($this->cfg["time"]) * 20;
-				$Item = $players->getInventory()->getItemInHand();
- 			$players->sendPopup(" §eID:§b $Name §cRank:§d $Group §aCoin:§f $Money §2$ \n §9Item: $Item");
+			$Item = $players->getInventory()->getItemInHand();
+ 			$players->sendPopup(" §eไอดี:§b $Name §cออนไลน์:§d $Online §aเงินที่มี:§f $Money §2$ \n §9ไอเท็ม: $Item");
  		}
  	}
 
 	public function onDisable() {
-		$this->getLogger()->info("§4Disabled !");
+		$this->getLogger()->info("§4ปลักอิน แสดงสถานะถูกปิด !");
 	}
 }
